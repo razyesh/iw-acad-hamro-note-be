d0cf52c00b3a436a422d23e773f2baa6e124fd5a
Aju100 <ajutamang10@outlook.com>
2020-08-09 22:21:01 +0545

    fix: managed secret in CI/CD🔒🔒🔒

--- backend/settings.py
+++ backend/settings.py
@@ -1,14 +1,15 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
 
 ALLOWED_HOSTS = []
 
