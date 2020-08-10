cd051609c27a632ab00e8562820c374b539a7223
Aju100 <ajutamang10@outlook.com>
2020-08-10 20:19:54 +0545

    fix: solved mini issue🐛🐛🐛

--- backend/settings.py
+++ backend/settings.py
@@ -6,12 +6,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config("SECRET_KEY")
-SECRET_KEY = 'ailfigg'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config('DEBUG',cast=bool)
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
 
 
 ALLOWED_HOSTS = []
