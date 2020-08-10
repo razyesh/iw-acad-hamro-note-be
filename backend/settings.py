06cce3ae2dd8f0017b2ccbe2d70ad814c22868f1
AnjalBam <anjalbam81@gmail.com>
2020-08-10 19:22:14 +0545

    fix:[Issue: #9] Fixed Typo

--- backend/settings.py
+++ backend/settings.py
@@ -6,10 +6,13 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+# SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'ailfigg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',cast=bool)
+# DEBUG = config('DEBUG',cast=bool)
+DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
