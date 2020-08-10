404d528941b650561ad7db0f6726b50685cbd0f4
Aju100 <ajutamang10@outlook.com>
2020-08-10 19:36:29 +0545

    fix: Solved issue

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User  = get_user_model()
 
 class Category(models.Model):
     name = models.CharField(max_length = 50)
