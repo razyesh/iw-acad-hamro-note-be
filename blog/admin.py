49f270c66f342b76c026016a8eaf157c32e94f61
Aju100 <ajutamang10@outlook.com>
2020-08-10 21:29:23 +0545

    Fix: Maintain PEP8 standard♻️♻️♻️

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category,Tag,Blog,Comment,Subscriber
+from .models import Category, Tag, Blog, Comment, Subscriber
 
 admin.site.register(Category)
 admin.site.register(Tag)
