20ab64a09af1ad81e1765fa63202b7d28a5120c0
rajesh <rajeshpandey2053@gmail.com>
2020-08-10 11:12:50 +0545

    feat: add profile and education model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Profile
+from .models import Education
+from .models import University
+from .models import Faculty
+from .models import College
 
-# Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Education)
+admin.site.register(University)
+admin.site.register(Faculty)
+admin.site.register(College)
--- accounts/models/__init__.py
+++ accounts/models/__init__.py
@@ -1,1 +1,2 @@
-from . import *
+from .profile import *
+from .education import *
