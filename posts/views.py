6d5d606199528c54725752b9447937be858a2ca5
AnjalBam <anjalbam81@gmail.com>
2020-08-11 19:54:23 +0545

    feat:[views] added pagination to listposts classes

--- posts/views.py
+++ posts/views.py
@@ -4,9 +4,11 @@ from rest_framework.generics import ListAPIView
 
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from .paginations import CustomPageNumberPagination
 
 
 class ListPosts(ListAPIView):
     http_method_names = [u'get']
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    pagination_class = CustomPageNumberPagination
