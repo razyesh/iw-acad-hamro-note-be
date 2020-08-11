8a4281a40b7485547193c0b91f261a02fc217179
AnjalBam <anjalbam81@gmail.com>
2020-08-11 19:44:00 +0545

    feat:[pagination] file creation

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.generics import ListAPIView
+
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+
+class ListPosts(ListAPIView):
+    http_method_names = [u'get']
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
