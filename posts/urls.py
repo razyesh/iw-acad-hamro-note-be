4cea38d6975a00f5febdaf35b0024cea6a1bc6a8
AnjalBam <anjalbam81@gmail.com>
2020-08-12 01:56:03 +0545

    feat:[urls]urls config for lists and comment apis

--- posts/urls.py
+++ posts/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'posts'
-urlpatterns = []
+urlpatterns = [
+    path('comments/list', views.ListComments.as_view(), name='comment_list'),
+    path('posts/list', views.ListPosts.as_view(), name='posts_list')
+]
