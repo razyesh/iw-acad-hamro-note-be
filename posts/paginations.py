0f7ee597ce86c6790fecb6a4232b38b30e134c1b
AnjalBam <anjalbam81@gmail.com>
2020-08-11 19:57:50 +0545

    feat:[pagination] added seperate custom pagination classes fro posts and comments

--- posts/paginations.py
+++ posts/paginations.py
@@ -1,6 +1,11 @@
 from rest_framework.pagination import PageNumberPagination
 
 
-class CustomPageNumberPagination(PageNumberPagination):
+class CustomPostsPagination(PageNumberPagination):
     page_size = 15
     max_page_size = 50
+
+
+class CustomCommentsPagination(PageNumberPagination):
+    page_size = 5
+    max_page_size = 20
