e427bd9b8790d86c33ac5e9730910b23973bf308
AnjalBam <anjalbam81@gmail.com>
2020-08-11 19:58:22 +0545

    feat:[views]added ListComment view and setup

--- posts/views.py
+++ posts/views.py
@@ -4,11 +4,18 @@ from rest_framework.generics import ListAPIView
 
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from .paginations import CustomPageNumberPagination
+from .paginations import CustomPostsPagination, CustomCommentsPagination
 
 
 class ListPosts(ListAPIView):
-    http_method_names = [u'get']
+    http_method_names = [u'get', ]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    pagination_class = CustomPageNumberPagination
+    pagination_class = CustomPostsPagination
+
+
+class ListComments(ListAPIView):
+    http_method_names = [u'get', ]
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    pagination_class = CustomCommentsPagination
