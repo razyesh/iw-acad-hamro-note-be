b1c26fc6134e0ce0967bdf10be59fe2f1d27c144
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-08-11 07:57:14 +0545

    feat: separate installed_app django_app and third_party apps

--- backend/settings.py
+++ backend/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',cast=bool)
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
