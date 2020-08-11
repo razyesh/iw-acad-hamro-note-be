ecce1f7dc2624ffa26191e1c9348ef846aec99ef
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-08-11 14:30:33 +0545

    fix: add name in url

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -4,7 +4,7 @@ from .authtoken_views import CustomObtainAuthToken
 from .views import UserRegistrationView
 
 urlpatterns = [
-    path('register', UserRegistrationView.as_view()),
-    path('login', CustomObtainAuthToken.as_view()),
-    path('user/profile', UserDetail.as_view())
+    path('register', UserRegistrationView.as_view(), name='user-register'),
+    path('login', CustomObtainAuthToken.as_view(), name='user-login'),
+    path('user/profile', UserDetail.as_view(), name='user-profile')
 ]
