9d6e9c8d40601a6dcf7355b7d4b3b2d65c0c798b
AnjalBam <anjalbam81@gmail.com>
2020-08-12 02:15:50 +0545

    feat:[refactor] added comments

--- posts/paginations.py
+++ posts/paginations.py
@@ -1,5 +1,9 @@
 from rest_framework.pagination import PageNumberPagination
 
+"""
+Custom Pagination classes to override the default behaviour.
+"""
+
 
 class CustomPostsPagination(PageNumberPagination):
     page_size = 15
