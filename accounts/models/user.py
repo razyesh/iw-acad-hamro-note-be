d8a59f6e0e51c356bf4637f015a99bff017e842c
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-08-13 16:27:03 +0545

    feat: custom user manager

--- accounts/models/user.py
+++ accounts/models/user.py
@@ -1,6 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(UserManager):
+    def _create_user(self, username, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not username:
+            raise ValueError('The given username must be set')
+        email = self.normalize_email(email)
+        username = self.model.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        if user.is_superuser:
+            user.is_active = True
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractUser):
@@ -11,6 +28,7 @@ class User(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+    objects = CustomUserManager()
 
     is_active = models.BooleanField(
         _('active'),
@@ -20,3 +38,6 @@ class User(AbstractUser):
             'Unselect this instead of deleting accounts.'
         ),
     )
+
+    def create_superuser(self):
+        super().create_superuser()
