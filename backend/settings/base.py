58f793f0c92783308e31449df53ff6abda2642f0
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-08-13 08:27:48 +0545

    fix: remove rest api settings from base

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -38,12 +38,6 @@ THIRD_PARTY_APPS = [
 
 INSTALLED_APPS = DJANGO_APPS + SYSTEM_APPS + THIRD_PARTY_APPS
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-    'rest_framework.permissions.AllowAny',
-    ]
-}
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
