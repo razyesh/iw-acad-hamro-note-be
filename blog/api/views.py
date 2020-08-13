e4cc08f2fcf085660264bb26679c515c0ea5d3b3
Aju100 <ajutamang10@outlook.com>
2020-08-13 10:47:05 +0545

    fix: update permissions

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,12 +1,16 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
 from blog.models import Blog
 from .serializers import BlogSerializer
 
-
 class BlogList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated, ]
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
 
 class BlogDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated, ]
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+
