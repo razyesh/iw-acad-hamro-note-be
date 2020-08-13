24652035c891db7f5e86229518d290b0c34cd0fd
AnjalBam <anjalbam81@gmail.com>
2020-08-13 19:54:57 +0545

    feat:[comments] the url for the crud operations mapped.

--- posts/serializers.py
+++ posts/serializers.py
@@ -23,4 +23,4 @@ class CreatePostSerializer(serializers.ModelSerializer):
 class CreateCommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['comment_description', ]
+        fields = ['post', 'user', 'comment_description', ]
--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'posts'
 urlpatterns = [
-    path('comment/list/', views.ListComments.as_view(), name='comment_list'),
+
     path('post/list/', views.ListPosts.as_view(), name='posts_list'),
     path('post/create/', views.CreatePost.as_view(), name='post_create'),
     # The below url can view and delete the post with get and delete method.
@@ -11,4 +11,12 @@ urlpatterns = [
          name='post_get_delete'),
     path('post/<str:post_slug>/update/', views.UpdatePost.as_view(),
          name='post_update'),
+    # comments
+    path('comment/list/', views.ListComments.as_view(), name='comment_list'),
+    path('comment/create/', views.CreateComment.as_view(),
+         name='comment_create'),
+    path('comment/<str:pk>/update/', views.UpdateComment.as_view(),
+         name='comment_update'),
+    path('comment/<str:pk>/', views.RetrieveDeleteComment.as_view(),
+         name='comment_get_delete'),
 ]
