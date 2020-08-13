4cb718064bc3523c32d20c633cb23745b25ae8a2
AnjalBam <anjalbam81@gmail.com>
2020-08-13 17:58:50 +0545

    feat:[comment_views]added crud for the comments

--- posts/views/comments.py
+++ posts/views/comments.py
@@ -1,6 +1,6 @@
 from .posts import *
 from ..models import Comment
-from ..serializers import CommentSerializer
+from ..serializers import CommentSerializer, CreateCommentSerializer
 from ..paginations import CustomCommentsPagination
 
 
@@ -15,3 +15,29 @@ class ListComments(ListAPIView):
     def get_queryset(self):
         return Comment.objects.all()
 
+
+class CreateComment(CreateAPIView):
+    """
+    This creates a new comment and saves to the database.
+    """
+    serializer_class = CreateCommentSerializer
+
+
+class UpdateComment(UpdateAPIView):
+    """
+    This should update the comment.
+    """
+    serializer_class = CreateCommentSerializer
+
+    def get_queryset(self):
+        return Comment.objects.all()
+
+
+class RetrieveDeleteComment(RetrieveDestroyAPIView):
+    """
+    This retrieves the comment with get request and deletes with delete request.
+    """
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        return Comment.objects.all()
