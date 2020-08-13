2dd131ff004f66ab30f6b9e7b9e3252ee1b7c6b0
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-08-13 13:37:33 +0545

    feat: activation url

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from .views import UserRegistrationView, UserDetail, UserUpdate
 from .authtoken_views import CustomObtainAuthToken
-from .views import UserRegistrationView
+from .views import UserRegistrationView, activate
+
+user_url = [
+    path('profile', UserDetail.as_view(), name='user-profile'),
+    path('update', UserUpdate.as_view(), name = 'user-update'),
+    path('activate/<uidb64>/<token>', activate, name='user-activate')
+]
+
 
 urlpatterns = [
     path('register', UserRegistrationView.as_view(), name='user-register'),
     path('login', CustomObtainAuthToken.as_view(), name='user-login'),
-    path('user/profile', UserDetail.as_view(), name='user-profile'),
-    path('user/update', UserUpdate.as_view(), name = 'user-update'),
+    path('user/', include(user_url))
 ]
